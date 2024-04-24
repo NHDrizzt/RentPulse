3d30b62df20a4316711d939b4aba889fa1f6fcf8
NHDrizzt <biel_s.carvalho2@hotmail.com>
2024-04-24 19:03:55 -0300

    Init project

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const metadata = {
+  title: "RentPulse | Your perfect Rental",
+  description: "Find your next rental property here",
+  keywords: "rental, property, rent, house, apartment, condo",
+};
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Rent Pulse</h1>
+      <p>Find your next rental property here</p>
+    </div>
+  );
+};
+
+export default HomePage;
